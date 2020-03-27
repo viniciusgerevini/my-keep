23287ebddd1510fd141a6c8b8ceb2ea433627579
viniciusgerevini <viniciusgerevini@users.noreply.github.com>
2020-03-27 11:03:38 +0000

    deploy: 061fb9e44c7911dd9c9f326629a58bf79315ddfe

---
+++ precache-manifest.c6d9db784916c3fa4a9682723bf27bd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc54555bac56f9be8a994416afdcc3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ea340fb0d1d23907962",
+    "url": "/static/css/2.7fe1ef02.chunk.css"
+  },
+  {
+    "revision": "1dab6a2eaabc7edf4b0b",
+    "url": "/static/css/main.e66c1cc0.chunk.css"
+  },
+  {
+    "revision": "1ea340fb0d1d23907962",
+    "url": "/static/js/2.db47d0d0.chunk.js"
+  },
+  {
+    "revision": "702e06c21f4db63e7e05a8a0eda9baf1",
+    "url": "/static/js/2.db47d0d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dab6a2eaabc7edf4b0b",
+    "url": "/static/js/main.db40cf37.chunk.js"
+  },
+  {
+    "revision": "b27d18313dd212a4d9d9",
+    "url": "/static/js/runtime-main.068808d9.js"
+  }
+]);
