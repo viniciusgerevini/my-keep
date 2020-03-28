491852ccac1ca738645374b5804a40a2d090d84b
Vinicius Gerevini <viniciusgerevini@gmail.com>
2020-03-28 12:28:02 +1100

    chore: move theme and store providers to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { useState } from 'react';
+import { createStore } from 'redux';
 import styled from 'styled-components';
-// import './App.css';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import { devToolsEnhancer } from 'redux-devtools-extension/logOnlyInProduction';
 
 import TopBar from './layout/components/TopBar';
 import SideBar from './layout/components/SideBar';
 import MainPane from './layout/components/MainPane';
+import reducers from './reducers';
+
+const theme = {
+  background: '#fff',
+  secondaryBackground: 'rgba(60,64,67,0.08)',
+  textColor: '#202124',
+  secondaryTextColor: '#80868b',
+  shadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
+  sidebarSize: '300px',
+};
+
+const initialState = {};
+
+const store = createStore(reducers, initialState, devToolsEnhancer());
+
 
 const AppWrapper = styled.div `
   height: 100%;
@@ -29,13 +47,17 @@ function App() {
   };
 
   return (
-    <AppWrapper>
-      <TopBar onSidebarButtonClicked={toggleSidebar} />
-      { isSidebarVisible ? <SideBar /> : '' }
-      <MainPaneWrapper isSidebarOpen={isSidebarVisible}>
-        <MainPane/>
-      </MainPaneWrapper>
-    </AppWrapper>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <AppWrapper>
+          <TopBar onSidebarButtonClicked={toggleSidebar} />
+          { isSidebarVisible ? <SideBar /> : '' }
+          <MainPaneWrapper isSidebarOpen={isSidebarVisible}>
+            <MainPane/>
+          </MainPaneWrapper>
+        </AppWrapper>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,37 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { ThemeProvider } from 'styled-components';
 
 import * as serviceWorker from './serviceWorker';
 import App from './App';
-import reducers from './reducers';
 
 import './reset.css';
 import './index.css';
 
-const theme = {
-  background: '#fff',
-  secondaryBackground: 'rgba(60,64,67,0.08)',
-  textColor: '#202124',
-  secondaryTextColor: '#80868b',
-  shadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
-  sidebarSize: '300px',
-};
-
-const initialState = {};
-
-const store = createStore(reducers, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
